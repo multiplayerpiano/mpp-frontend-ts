ed3d5a8fb9c342359ab506d0a1a32699ce01cf1d
Bop It Freak <augustberchelmann@gmail.com>
2021-07-19 22:43:07 -0400

    ebsprite

--- src/Misc/EbspriteManager.ts
+++ src/Misc/EbspriteManager.ts
@@ -1,20 +1,22 @@
 import { MPP } from "..";
 import { MultiplayerPianoClient } from "../MultiplayerPianoClient";
-import "./ebsprite.js";
+import { Ebsprite } from "./ebsprite";
 
 export class EbspriteManager {
 	gInterface: MultiplayerPianoClient;
+	ebsprite: Ebsprite;
   constructor(gInterface: MultiplayerPianoClient) {
 		this.gInterface = gInterface;
+		this.ebsprite = new Ebsprite();
     gInterface.clientManager.gClient.on("ch", this.eb.bind(this));
     this.eb();
   }
   eb() {
 		//using window as a temporary solution. Will be removed when ebsprite is typescriptified
 		if(this.gInterface.clientManager.gClient.channel && this.gInterface.clientManager.gClient.channel._id.toLowerCase() === "test/fishing") {
-			(window as any).ebsprite.start(this.gInterface.clientManager.gClient); 
+			this.ebsprite.start(this.gInterface.clientManager.gClient); 
 		} else {
-			(window as any).ebsprite.stop();
+			this.ebsprite.stop();
 		}
 	}
 }
